b97e717ac82032cb39eba4d2536f8f1ffae7f2af
leonardo perez <andresppu@hotmail.com>
2024-07-22 00:12:56 -0500

    fin version 5.0.0

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,3 +1,4 @@
+const { Categoria, Producto } = require("../models");
 const Role = require("../models/role");
 const Usuario=require("../models/usuario");
 
@@ -22,5 +23,17 @@ const existeUsuarioPorId = async(id)=>{
         throw new Error(` el id:  ${id} no existe`);
     }
 }
+const existeCategoriaPorId = async(id)=>{
+    const existeCategoria= await Categoria.findById(id);
+    if (!existeCategoria) {
+        throw new Error(` el id:  ${id} no existe`);
+    }
+}
+const existeProductoPorId = async(id)=>{
+    const existeProducto= await Producto.findById(id);
+    if (!existeProducto) {
+        throw new Error(` el producto con id:  ${id} no existe`);
+    }
+}
 
-module.exports={esRolValido,emailExiste,existeUsuarioPorId};
+module.exports={esRolValido,emailExiste,existeUsuarioPorId,existeCategoriaPorId,existeProductoPorId};
